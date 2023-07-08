9c88c1fc9cf43a95238cc0fed48bc43571df0d98
Lauren Sisson <91545568+Mommyofpotato@users.noreply.github.com>
2023-07-08 14:59:19 -0400

    Add files via upload

---
+++ PlayerControllerX.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public bool timeOut = true;
+    public float relax = 1.5f;
+    // Update is called once per frame
+    void Update()
+    {// On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timeOut == true)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            private float timer += Time.deltaTime;
+        }
+if (timer >= relax)
+{
+    timeOut = true;
+    timer = 0f;
+}
+else
+{
+    timeOut = false;
+}
+    } 
+}
+
+
+
+        
+
+
+         
+
+
+
+
